c961b96c9657e39c57af06c44e1c68fd8e4f9032
Cheonse <108046230+cheonseunghyeon@users.noreply.github.com>
2023-06-24 13:42:57 +0900

    게시물작성

---
+++ components/CameraButton.js
@@ -0,0 +1,106 @@
+import React, {useState} from 'react';
+import {View, Pressable, StyleSheet, Platform} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import UploadModeModal from './UploadModeModal';
+import {useNavigation} from '@react-navigation/native';
+import {launchImageLibrary, launchCamera} from 'react-native-image-picker';
+
+const TABBAR_HEIGHT = 49;
+
+const imagePickerOption = {
+  mediaType: 'photo',
+  maxWidth: 768,
+  maxHeight: 768,
+  includeBase64: Platform.OS === 'android',
+};
+
+function CameraButton() {
+  const insets = useSafeAreaInsets();
+  const [modalVisible, setModalVisible] = useState(false);
+
+  // 네비로 이동할 수 있게 해줌
+  const navigation = useNavigation();
+
+  // 이미지 선택 시 포스트 작성 화면의 res를 라우트 파라미터로 넣어서 열기
+  // res - 선택한 이미지 정보
+  const onPickImage = res => {
+    if (res.didCancel || !res) {
+      return;
+    }
+    navigation.push('Upload', {res});
+  };
+  // 카메라 촬영과 이미지 선택 모두 같은 옵션이라 상수로 정의
+  const onLaunchCamera = () => {
+    launchCamera(imagePickerOption, onPickImage);
+  };
+
+  const onLaunchImageLibrary = () => {
+    launchImageLibrary(imagePickerOption, onPickImage);
+  };
+
+  // 기종에 따라 다른 화면
+  const bottom = Platform.select({
+    android: TABBAR_HEIGHT / 2,
+    ios: TABBAR_HEIGHT / 2 + insets.bottom - 4,
+  });
+
+  return (
+    <>
+      <View style={[styles.wrapper, {bottom}]}>
+        <Pressable
+          android_ripple={{
+            color: '#ffffff',
+          }}
+          style={styles.circle}
+          onPress={() => setModalVisible(true)}>
+          <Icon name="camera-alt" color="white" size={24} />
+        </Pressable>
+      </View>
+      <UploadModeModal
+        visible={modalVisible}
+        onClose={() => setModalVisible(false)}
+        onLaunchCamera={onLaunchCamera}
+        onLaunchImageLibrary={onLaunchImageLibrary}
+      />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    zIndex: 5,
+    borderRadius: 27,
+    height: 54,
+    width: 54,
+    position: 'absolute',
+    left: '50%',
+    transform: [
+      {
+        translateX: -27,
+      },
+    ],
+    ...Platform.select({
+      ios: {
+        shadowColor: '#4d4d4d',
+        shadowOffset: {width: 0, height: 4},
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 5,
+        overflow: 'hidden',
+      },
+    }),
+  },
+  circle: {
+    backgroundColor: '#6200ee',
+    borderRadius: 27,
+    height: 54,
+    width: 54,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default CameraButton;
