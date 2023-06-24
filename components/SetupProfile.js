b815e6a759378f497a28d426fbf5354f497f5f1d
Cheonse <108046230+cheonseunghyeon@users.noreply.github.com>
2023-06-24 12:33:03 +0900

    8장

---
+++ components/SetupProfile.js
@@ -0,0 +1,146 @@
+import {useNavigation, useRoute} from '@react-navigation/native';
+import React, {useState} from 'react';
+
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  View,
+  ActivityIndicator,
+  Platform,
+} from 'react-native';
+// 이미지 가져오기
+import {launchImageLibrary} from 'react-native-image-picker';
+// firebase에 저장
+import storage from '@react-native-firebase/storage';
+import {signOut} from '../lib/auth';
+import {createUser} from '../lib/users';
+import BorderedInput from './BorderedInput';
+import CustomButton from './CustomButton';
+import {useUserContext} from '../contexts/UserContext';
+
+function SetupProfile() {
+  const [displayName, setDisplayName] = useState('');
+  const navigation = useNavigation();
+  // 유저 정보 조회
+  const {setUser} = useUserContext();
+  // 이미지 uri 관리
+  const [response, setResponse] = useState(null);
+  const {params} = useRoute();
+
+  const [loading, setLoading] = useState(false);
+  const {uid} = params || {};
+
+  const onSubmit = async () => {
+    setLoading(true);
+
+    let photoURL = null;
+
+    if (response) {
+      const asset = response.assets[0];
+      const extension = asset.fileName.split('.').pop(); // 확장자 추출
+      const reference = storage().ref(`/profile/${uid}.${extension}`);
+
+      if (Platform.OS === 'android') {
+        //안드로이드의 경우 base64로 인코딩된 데이터를 업로드
+        await reference.putString(asset.base64, 'base64', {
+          contentType: asset.type,
+        });
+      } else {
+        await reference.putFile(asset.uri);
+      }
+
+      photoURL = response ? await reference.getDownloadURL() : null;
+    }
+    // 프로필 정보
+    const user = {
+      id: uid,
+      displayName,
+      photoURL,
+    };
+    // 정보에 따라 프로필 생성 후 조회
+    createUser(user);
+    setUser(user);
+    console.log(user);
+  };
+  const onCancel = () => {
+    signOut();
+    navigation.goBack();
+  };
+  const onSelectImage = () => {
+    // callback 함수 - 이미지 호출
+    launchImageLibrary(
+      {
+        mediaType: 'photo',
+        maxWidth: 512,
+        maxHeight: 512,
+        // 안드로이드일 경우  includeBase64가 true - uri 권한 오류 발생 위험
+        includeBase64: Platform.OS === 'android',
+      },
+      res => {
+        if (res.didCancel) {
+          // 취소했을 경우
+          return;
+        }
+        setResponse(res);
+      },
+    );
+  };
+
+  return (
+    <View style={styles.block}>
+      <Pressable onPress={onSelectImage}>
+        <Image
+          style={styles.circle}
+          source={
+            response
+              ? {uri: response?.assets[0]?.uri}
+              : require('../assets/user.png')
+          }
+        />
+      </Pressable>
+      <View style={styles.form}>
+        <BorderedInput
+          placeholder="닉네임"
+          value={displayName}
+          onChangeText={setDisplayName}
+          onSubmitEditing={onSubmit}
+          returnKeyType="next"
+        />
+        {/* 로딩 중이라면 버튼 대신 회전 연출 */}
+        {loading ? (
+          <ActivityIndicator size={32} color="#6200ee" style={styles.spinner} />
+        ) : (
+          <View style={styles.buttons}>
+            <CustomButton title="다음" onPress={onSubmit} hasMarginBottom />
+            <CustomButton title="취소" onPress={onCancel} theme="secondary" />
+          </View>
+        )}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  block: {
+    alignItems: 'center',
+    marginTop: 24,
+    paddingHorizontal: 16,
+    width: '100%',
+  },
+  circle: {
+    backgroundColor: '#cdcdcd',
+    borderRadius: 64,
+    width: 128,
+    height: 128,
+  },
+  form: {
+    marginTop: 16,
+    width: '100%',
+  },
+  buttons: {
+    marginTop: 48,
+  },
+});
+
+export default SetupProfile;
---
+++ screens/SignInScreen.js
@@ -0,0 +1,119 @@
+import React, {useState} from 'react';
+import {
+  Alert,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import SignButtons from '../components/SignButtons';
+import SignInForm from '../components/SignForm';
+import {signIn, signUp} from '../lib/auth';
+import {getUser} from '../lib/users';
+import {useUserContext} from '../contexts/UserContext';
+
+function SignInScreen({navigation, route}) {
+  const {isSignUp} = route.params || {};
+
+  // 로그인 정보 객체로 저장
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  // 로딩 상태
+  const [loading, setLoading] = useState();
+
+  // 유저 정보 조회
+  const {setUser} = useUserContext();
+
+  const createChangeTextHandler = name => value => {
+    setForm({...form, [name]: value});
+  };
+  const onSubmit = async () => {
+    Keyboard.dismiss();
+
+    const {email, password, confirmPassword} = form;
+
+    if (isSignUp && password !== confirmPassword) {
+      Alert.alert('실패', '비밀번호가 일치하지 않습니다.');
+      console.log({password, confirmPassword});
+      return;
+    }
+
+    setLoading(true);
+    const info = {email, password};
+
+    try {
+      const {user} = isSignUp ? await signUp(info) : await signIn(info);
+      const profile = await getUser(user.uid);
+      // 프로필 상태가 존재하는지 여부에 따라 화면 구성
+
+      if (!profile) {
+        navigation.navigate('Welcome', {uid: user.uid});
+      } else {
+        // 프로필 조회
+        setUser(profile);
+      }
+    } catch (e) {
+      const messages = {
+        'auth/email-already-in-use': '이미 가입된 이메일입니다.',
+        'auth/wrong-password': '잘못된 비밀번호입니다.',
+        'auth/user-not-found': '존재하지 않는 계정입니다.',
+        'auth/invalid-email': '유효하지 않은 이메일 주소입니다.',
+      };
+      const msg = messages[e.code] || `${isSignUp ? '가입' : '로그인'} 실패`;
+      Alert.alert('실패', msg);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <KeyboardAvoidingView
+      style={styles.keyboardAvoidingView}
+      behavior={Platform.select({ios: 'padding'})}>
+      <SafeAreaView style={styles.fullscreen}>
+        <Text style={styles.text}>PublicGallery</Text>
+        <View style={styles.form}>
+          <SignInForm
+            isSignUp={isSignUp}
+            onSubmit={onSubmit}
+            form={form}
+            createChangeTextHandler={createChangeTextHandler}
+          />
+          <SignButtons
+            isSignUp={isSignUp}
+            onSubmit={onSubmit}
+            loading={loading}
+          />
+        </View>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  keyboardAvoidingView: {
+    flex: 1,
+  },
+  fullscreen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  form: {
+    marginTop: 64,
+    width: '100%',
+    paddingHorizontal: 16,
+  },
+});
+
+export default SignInScreen;
